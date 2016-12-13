b1f5fb3597665b6e1e64ecc3edb79654878ea613
Ivano Rotondo <ivano.rotondo@gmail.com>
2016-12-13 11:58:47 +0000

    pieces added and now showing on the chessboard

--- Chess/Chessboard.swift
+++ Chess/Chessboard.swift
@@ -13,13 +13,67 @@ class Chessboard: UIView {
     
     @IBInspectable var fillColor: UIColor = UIColor.greenColor()
     
+    var chess = Chess()
+    var squareLength : CGFloat!
     
     override func drawRect(rect: CGRect) {
         
-        let squareLength = bounds.width/8
+        squareLength = bounds.width/8
+
+        chess.initialSetUp()
         
 //        drawLines(squareLength)
         drawSquares(squareLength)
+        drawPieces(chess.chessboard)
+    }
+    
+    
+    private func drawPieces(chessboard: [[Int]]) {
+        
+        for row in 1...8 {
+            for col in 1...8{
+                let rect = getRectOfPosition(row, col: col)
+                let pieceImage = UIImageView(frame: rect)
+                let piece = chessboard[row - 1][col - 1]
+                
+                switch piece {
+                case 1: pieceImage.image = UIImage(named: "PawnWhite.png")
+                    break
+                case 2: pieceImage.image = UIImage(named: "RookWhite.png")
+                    break
+                case 3: pieceImage.image = UIImage(named: "KnightWhite.png")
+                    break
+                case 4: pieceImage.image = UIImage(named: "BishopWhite.png")
+                    break
+                case 5: pieceImage.image = UIImage(named: "QueenWhite.png")
+                    break
+                case 6: pieceImage.image = UIImage(named: "KingWhite.png")
+                    break
+                case 11: pieceImage.image = UIImage(named: "PawnBlack.png")
+                    break
+                case 22: pieceImage.image = UIImage(named: "RookBlack.png")
+                    break
+                case 33: pieceImage.image = UIImage(named: "KnightBlack.png")
+                    break
+                case 44: pieceImage.image = UIImage(named: "BishopBlack.png")
+                    break
+                case 55: pieceImage.image = UIImage(named: "QueenBlack.png")
+                    break
+                case 66: pieceImage.image = UIImage(named: "KingBlack.png")
+                    break
+                default:
+                    break
+                }
+                addSubview(pieceImage)
+            }
+        }
+    }
+    
+    
+    private func getRectOfPosition(row: Int, col: Int) -> CGRect {
+        let x = (CGFloat(col - 1)) * squareLength
+        let y = (CGFloat(8 - row)) * squareLength
+        return  CGRect(x: x, y: y, width: squareLength, height: squareLength)
     }
     
     
@@ -35,9 +89,8 @@ class Chessboard: UIView {
         for row in 1...8 {
             for col in 1...8{
                 if (isEven(row) && isEven(col)) || (!isEven(row) && !isEven(col)) {
-                    let x = (CGFloat(8 - row)) * squareLength
-                    let y = (CGFloat(col - 1)) * squareLength
-                    let square = CGRect(x: x, y: y, width: squareLength, height: squareLength)
+                    
+                    let square = getRectOfPosition(row, col: col)
                     var squareBP:UIBezierPath = UIBezierPath(rect: square)
                     
                     UIColor.blackColor().setStroke()
